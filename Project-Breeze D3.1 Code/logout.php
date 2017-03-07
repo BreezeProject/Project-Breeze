8ea3467eb7888cbd200f3ee498e6d55a4d0082d7
strickland123456789 <strickland123456789@gmail.com>
2017-03-07 10:41:23 -0700

    Revert "Source code added"
    This reverts commit 98b0cda39a0e2434c9d789853531f0c24e313a44.

--- Project-Breeze D3.1 Code/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
- session_start();
-mysql_query("UPDATE 'useractivity' SET 'userlogout' = now() WHERE 'userid'=" .$_SESSION['user']);
- session_destroy();
- header("Location: index.php");
- ?>
